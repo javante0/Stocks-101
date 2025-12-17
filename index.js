62279285f5e24e04142b8cc27f558522e866514c
John Avante <johnavante7567@gmail.com>
2025-12-16 20:15:52 -0500

    feat: initializing API

--- index.js
+++ index.js
@@ -10,6 +10,11 @@ dotenv.config();
 app.use(bodyParser.json())
 app.use(express.static(__dirname + '/public'));
 
+// Initialize Supabase Client
+const supabaseUrl = process.env.SUPABASE_URL;
+const supabaseKey = process.env.SUPABASE_KEY;
+const supabase = supabaseClient.createClient(supabaseUrl, supabaseKey);
+
 app.listen(port, () =>{
     console.log('App is available on port', port);
 });
